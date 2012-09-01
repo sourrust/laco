7f3a0641abf1269dec5d12699dc1f2ed13500ed8
Jeremy Hull <sourdrums@gmail.com>
2012-08-31 17:36:15 -0700

    Change to new interface in main

--- src/main.c
+++ src/main.c
@@ -1,28 +1,14 @@
-#include <lua.h>
-#include <lualib.h>
-#include <lauxlib.h>
-
-#include "commands.h"
+#include "laco.h"
 #include "util.h"
 
 int main(int argc, const char** argv) {
-  lua_State* L = luaL_newstate();
-
-  if(L == NULL) {
-    puts("Couldn't initialize lua state");
-    return -1;
-  }
-
-  if(argc > 1) {
-    laco_handleFlag(L, argv[1]);
-  }
-
-  luaL_openlibs(L);
+  struct LacoState* state = laco_newLacoState(argc, argv);
+  struct lua_State* L = laco_getLuaState(state);
 
   while(laco_loadline(L) != -1) {
     laco_handleline(L);
   }
 
-  lua_close(L);
+  laco_deleteLacoState(state);
   return 0;
 }
