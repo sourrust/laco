3cc9cc87b72dcfe5b74f3bf5b3fee4677694e05c
Jeremy Hull <sourdrums@gmail.com>
2012-09-04 22:42:56 -0700

    Add comments to header files

--- src/laco.h
+++ src/laco.h
@@ -3,6 +3,7 @@
 
 struct lua_State;
 
+/* State of laco REPL */
 struct LacoState {
   struct lua_State* L;
   const char* version;
@@ -13,7 +14,23 @@ struct LacoState {
   int status;
 };
 
+/**
+ * Initilazation of laco's state
+ *
+ * param          pointer to LacoState
+ * param          argument count (argc)
+ * param          arguments      (argv)
+ */
 void laco_initLacoState(struct LacoState*, int, const char**);
+
+/**
+ * Destroys the internal variable, but doesn't try to free the LacoState
+ * pointer itself.
+ *
+ * param          pointer to LacoState
+ *
+ * return 1 if pointer isn't NULL
+ */
 int laco_destroyLacoState(struct LacoState*);
 
 #endif /* __LACO_H */
