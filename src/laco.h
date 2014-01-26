a91d1e7ba1332bad5a735c067c487ab0c9d0eb9d
Jeremy Hull <sourdrums@gmail.com>
2014-01-25 20:27:29 -0700

    Add documentation for new get and set functions

--- src/laco.h
+++ src/laco.h
@@ -26,13 +26,48 @@ struct LacoState* laco_newLacoState(int, const char**);
  */
 int laco_destroyLacoState(struct LacoState*);
 
+/**
+ * Gets the lua state held with laco.
+ *
+ * param          pointer to LacoState
+ *
+ * return a pointer to lua_State
+ */
 struct lua_State* laco_getLacoLuaState(struct LacoState*);
 
+/**
+ * Gets the laco version held with laco.
+ *
+ * param          pointer to LacoState
+ *
+ * return a string of characters representing laco's version number
+ */
 const char* laco_getLacoVersion(struct LacoState*);
 
+/**
+ * Gets the arraying of arguments passed to laco.
+ *
+ * param          pointer to LacoState
+ *
+ * return an array of characters string passes into laco
+ */
 const char** laco_getLacoArgs(struct LacoState*);
 
+/**
+ * Get the current status of the laco repl.
+ *
+ * param          pointer to LacoState
+ *
+ * return the current status of laco
+ */
 int laco_getLacoStatus(struct LacoState*);
+
+/**
+ * Set the current status of laco.
+ *
+ * param          pointer to LacoState
+ * param          status to be set
+ */
 void laco_setLacoStatus(struct LacoState*, int);
 
 #endif /* __LACO_H */
