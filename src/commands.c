ddf88fda672bf43c057eca80334d04be837ad336
Jeremy Hull <sourdrums@gmail.com>
2015-07-03 16:51:20 -0700

    Use laco_is_match for command handling

--- src/commands.c
+++ src/commands.c
@@ -1,9 +1,12 @@
 #include "commands.h"
 
+#include <stdbool.h>
 #include <stdio.h>
-#include <string.h>
 
 #include "util.h"
+
+static const char* quit_matches[] = {"quit"};
+static const char* help_matches[] = {"help"};
 
 static void print_commands_help() {
   puts("  Commands available:\n");
@@ -11,13 +14,21 @@ static void print_commands_help() {
   puts("    :help   \tDisplay this list of commands");
 }
 
+static inline bool is_quit(const char* command) {
+  return laco_is_match(quit_matches, command);
+}
+
+static inline bool is_help(const char* command) {
+  return laco_is_match(help_matches, command);
+}
+
 void handle_command(struct LacoState* laco, char* line) {
   if(laco != NULL && line != NULL) {
     const char* command = line + 1;
 
-    if(strcmp(command, "quit") == 0) {
+    if(is_quit(command)) {
       laco_kill(laco, 0, "Exiting laco...");
-    } else if(strcmp(command, "help") == 0)  {
+    } else if(is_help(command))  {
       print_commands_help();
     }
 
