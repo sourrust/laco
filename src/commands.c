ceed3a0b2d26959c67a58ecc55e59bf5f0deb362
Jeremy Hull <sourdrums@gmail.com>
2015-07-02 18:15:29 -0700

    Add help screen for REPL commands

--- src/commands.c
+++ src/commands.c
@@ -1,8 +1,15 @@
 #include "commands.h"
 
+#include <stdio.h>
 #include <string.h>
 
 #include "util.h"
+
+static void print_commands_help() {
+  puts("  Commands available:\n");
+  puts("    :quit   \tExit laco");
+  puts("    :help   \tDisplay this list of commands");
+}
 
 void handle_command(struct LacoState* laco, char* line) {
   if(laco != NULL && line != NULL) {
@@ -10,6 +17,8 @@ void handle_command(struct LacoState* laco, char* line) {
 
     if(strcmp(command, "quit") == 0) {
       laco_kill(laco, 0, "Exiting laco...");
+    } else if(strcmp(command, "help") == 0)  {
+      print_commands_help();
     } else {
       /* Make it seem like this was an empty line */
       line[0] = '\0';
